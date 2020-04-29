09aff4061b127b64a51b598ffbdc50c192328d98
MilanP5G <milan_parmar@hotmail.co.uk>
2020-04-29 19:35:57 +0100

    Done.

---
+++ db/migrate/001_create_coupons_table.rb
@@ -0,0 +1,12 @@
+class CreateCouponsTable < ActiveRecord::Migration
+
+  def change
+    create_table :coupons do | t |
+      t.string :coupon_code
+      t.string :store
+
+      t.timestamps :null
+    end
+  end
+
+end
