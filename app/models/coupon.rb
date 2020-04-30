ad52bec9adc92a22f4427613e10e5e45878e3ab8
8ceg6 <8ceg6@comcast.net>
2020-04-30 14:00:58 -0400

    Done.

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,6 +1,6 @@
 class Coupon < ApplicationRecord
 
-    # def full_coupon 
-    #     self.coupon_code "-"  self.store 
-    # end 
+    def full_coupon 
+        self.coupon_code "-"  self.store 
+    end 
 end
